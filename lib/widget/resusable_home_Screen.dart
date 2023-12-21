2fec50d62f7159c9b8b02a1027c8cad53eaa188b
len <dvar4890@gmail.com>
2023-12-21 09:50:23 +0700

    Update overflow AuthScreen

---
+++ lib/widget/resusable_home_Screen.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class TextLogoSerach extends StatelessWidget {
+  TextLogoSerach({
+    super.key,
+    required this.mainText,
+    required this.width,
+  });
+  String mainText;
+  double? width;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      // "Delicious",
+      mainText,
+      style: TextStyle(
+          color: Colors.black,
+          fontSize: width,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'ConcertOne'),
+    );
+  }
+}
